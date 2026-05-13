83409a5e4d9dfc02f9604fda5cd00198222b4d75
Antoine Libert <contact@antoine-libert.com>
2026-05-13 08:57:42 +0200

    clean : remove useless space

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ set -e
 RESTICPROFILE_CONFIG =${RESTICPROFILE_CONFIG :-/resticprofile/profiles.yml}
 
 echo "[entrypoint] Registering schedules..."
-resticprofile -c "$RESTICPROFILE_CONFIG " schedule --all
+resticprofile -c "$RESTICPROFILE_CONFIG" schedule --all
 
 echo "[entrypoint] Starting supercronic..."
 exec supercronic /resticprofile/crontab
