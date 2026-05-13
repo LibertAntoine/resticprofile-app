5305d8aff73e1ac8cae760a5608de19b7cf72813
Antoine Libert <contact@antoine-libert.com>
2026-05-13 09:08:38 +0200

    fix: yaml file extension

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 
-RESTICPROFILE_CONFIG =${RESTICPROFILE_CONFIG :-/resticprofile/profiles.yml}
+RESTICPROFILE_CONFIG =${RESTICPROFILE_CONFIG :-/resticprofile/profiles.yaml}
 
 echo "[entrypoint] Registering schedules..."
 resticprofile -c "$RESTICPROFILE_CONFIG" schedule --all
